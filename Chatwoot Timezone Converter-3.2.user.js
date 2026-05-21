793d5b44fbb89cb73f5995b62eca9c41852974d8
Qweb Design <qwebdesing@inbox.ru>
2026-05-21 20:41:04 +0300

    test

---
+++ Chatwoot Timezone Converter-3.2.user.js
@@ -0,0 +1,40 @@
+// ==UserScript==
+// @name         ACID CW PERKS
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  CWP ACID perks
+// @author       Marro Denitsia
+// @match        *://chatwoot.echelon.su/*
+// @match        *://echelon.su/api/dashboard/*
+// @updateURL    https://openuserjs.org/meta/Denmar/Chatwoot_Timezone_Converter.meta.js
+// @downloadURL  https://openuserjs.org/install/Denmar/Chatwoot_Timezone_Converter.user.js
+// @grant        none
+// ==/UserScript==
+
+(function () {
+    'use strict';
+
+    setInterval(() => {
+        const spans = document.querySelectorAll('span.font-mono');
+
+        for (const span of spans) {
+            if (span.getAttribute('data-timezone-updated') === 'true') continue;
+
+            const text = span.textContent.trim();
+            const timeMatch = text.match(/^(\d{1,2}):(\d{2}):(\d{2})$/);
+
+            if (timeMatch) {
+                let hours = parseInt(timeMatch[1], 10);
+                const minutes = timeMatch[2];
+                const seconds = timeMatch[3];
+
+                hours = (hours + 3) % 24;
+                const formattedHours = hours.toString().padStart(2, '0');
+
+                span.textContent = `[${formattedHours}:${minutes}:${seconds}]`;
+                span.style.setProperty('font-size', '65%', 'important');
+                span.setAttribute('data-timezone-updated', 'true');
+            }
+        }
+    }, 500);
+})();
